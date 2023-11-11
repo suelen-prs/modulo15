8f5a1a3c804f26c91a9c85e7a44180b49d758934
Suélen Peres <suelen.prs@gmail.com>
2023-11-10 22:56:11 -0300

    enviando exercicio 1

---
+++ exercicio1_parte2.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors].''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
+
+md = st.text_area('Type in your markdown string (without outer quotes)',
+                  "Happy Streamlit-ing! :balloon:")
+
+st.code(f"""
+import streamlit as st
+
+st.markdown('''{md}''')
+""")
+
+st.markdown(md)
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+
+
+st.header('This is a header with a divider', divider='rainbow')
+st.header('_Streamlit_ is :blue[cool] :sunglasses:')
+
